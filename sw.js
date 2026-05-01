8aeea3c43a53c4f1daa0d009957c69ed5daa7ed7
niedzwieckimn-ctrl <niedzwiecki.mn@gmail.com>
2026-05-01 12:23:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker zainstalowany");
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {});
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
